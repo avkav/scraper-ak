637c40fc48f651d1641231e89de18a281c2cdbea
avkav <avkavrecic@gmail.com>
2024-07-29 12:14:44 +0200

    Se actualiza db

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 DB_NAME = "quotes_db"
 DB_USER = "postgres"  # Reemplaza con tu usuario de PostgreSQL #test
-DB_PASSWORD = "postgres"  # Reemplaza con tu contraseña de PostgreSQL #123
+DB_PASSWORD = "postgres"  # Reemplaza con tu contraseña de PostgreSQL
 DB_HOST = "localhost"  # Cambia si tu host es diferente
 DB_PORT = 5432
